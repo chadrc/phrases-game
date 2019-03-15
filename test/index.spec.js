364e8795cd3b28bf52d8fa553b3990cf186d3a96
chadcollins <chadrcollins91@gmail.com>
2019-03-15 06:55:44 -0400

    Setup placeholder test so it passes

---
+++ test/index.spec.js
@@ -0,0 +1,20 @@
+// import { mount, createLocalVue } from "@vue/test-utils";
+// import Vuex from "vuex";
+// import SignUpModal from "@/components/SignUpModal.vue";
+
+describe("Index", () => {
+  test("placeholder", () => {
+    //   const localVue = createLocalVue();
+    //   localVue.use(Vuex);
+    //   // const routes = [{ path: "/foo", component: Foo }];
+    //   // const router = new VueRouter({
+    //   //   routes
+    //   // });
+    //   const wrapper = mount(SignUpModal, {
+    //     localVue
+    //     // router
+    //   });
+    //   // const wrapper = mount(SignUpModal);
+    expect(2 + 2).toBe(4);
+  });
+});
