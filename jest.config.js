df67d6ae58d1013b224d91a97b3d3af3e1213b76
chadcollins <chadrcollins91@gmail.com>
2019-03-16 08:03:13 -0400

    Added services and store to coverage

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     "<rootDir>/components/**/*.vue",
-    "<rootDir>/pages/**/*.vue"
-  ],
+    "<rootDir>/pages/**/*.vue",
+    "<rootDir>/store/**/*.js",
+    "<rootDir>/services/**/*.js"
+  ]
 };
