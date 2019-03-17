277ea385132292029cda02c9c5fb7ef42f85a670
chadcollins <chadrcollins91@gmail.com>
2019-03-17 16:29:53 -0400

    Added auth check to startGame

--- services/GameService.js
+++ services/GameService.js
@@ -2,8 +2,21 @@ import uuid from "uuid/v4";
 
 export default class GameService {
   _games = {};
+  _authService = null;
+
+  constructor(authService) {
+    this._authService = authService;
+  }
 
   startGame() {
+    if (!this._authService.currentAccessToken) {
+      return Promise.resolve({
+        error: {
+          message: "log in to start a game"
+        }
+      });
+    }
+
     const newGame = {
       id: uuid(),
       word: "Hello",
--- test/services/GameService.spec.js
+++ test/services/GameService.spec.js
@@ -2,7 +2,11 @@ import GameService from "@/services/GameService";
 
 describe("GameService", () => {
   test("startGame", async () => {
-    const gameService = new GameService();
+    const gameService = new GameService({
+      get currentAccessToken() {
+        return "123";
+      }
+    });
 
     const startGameResponse = await gameService.startGame();
 
@@ -10,5 +14,13 @@ describe("GameService", () => {
     expect(startGameResponse.word).toBeTruthy();
     expect(startGameResponse.characterGuesses).toEqual([]);
     expect(startGameResponse.wordGuesses).toEqual([]);
+  });
+
+  test("startGame - no access", async () => {
+    const gameService = new GameService({});
+
+    const startGameResponse = await gameService.startGame();
+
+    expect(startGameResponse.error.message).toEqual("log in to start a game");
   });
 });
