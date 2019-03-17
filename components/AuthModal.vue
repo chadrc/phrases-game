d1ca87f751f48a2001c1a299b376d477381fa39d
chadcollins <chadrcollins91@gmail.com>
2019-03-17 12:23:13 -0400

    Added click to auth modal background to cancel

--- components/AuthModal.vue
+++ components/AuthModal.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="modal" :class="{'is-active': active}">
-  <div class="modal-background"></div>
+  <div class="modal-background" @click="cancel()"></div>
   <div class="modal-card">
     <header class="modal-card-head">
       <p class="modal-card-title">{{ title }}</p>
