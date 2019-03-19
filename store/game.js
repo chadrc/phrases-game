2def12b885417d42940bcf95f2ec74d0d4668762
chadcollins <chadrcollins91@gmail.com>
2019-03-19 07:01:48 -0400

    stub files for game actions

---
+++ store/game.js
@@ -0,0 +1,13 @@
+import GameService from "@/services/GameService";
+
+export const state = () => ({});
+
+export const getters = {};
+
+export const mutations = {};
+
+export const makeActions = gameService => {
+  return {};
+};
+
+export const actions = makeActions(new GameService());
---
+++ test/store/game/actions.spec.js
@@ -0,0 +1,3 @@
+// import { makeActions } from "@/store/game";
+
+describe("Game Actions", () => {});
