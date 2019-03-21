f10a70f62c2cd49c21ae3875d083652bbc6cfe88
chadcollins <chadrcollins91@gmail.com>
2019-03-20 20:17:05 -0400

    Page view spec now shallow renderingˆ

--- test/pages/play.spec.js
+++ test/pages/play.spec.js
@@ -2,7 +2,7 @@ import PlayView from "@/pages/play.vue";
 import { makeVueMock } from "@/test/utils";
 
 describe("Play View", () => {
-  const { wrapper } = makeVueMock(PlayView);
+  const { wrapper } = makeVueMock(PlayView, { shallow: true });
 
   test("is vue instance", () => {
     expect(wrapper.isVueInstance()).toBeTruthy();
